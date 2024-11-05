082d1c02ccc5e66465b0371c490e5da9ff209208
Shesh Ghimire <sheshgh@outlook.com>
2024-11-05 14:25:52 +0545

    Moved `EventBuilder::for()` logic to `EventType` enum. Updated `EventBuilder::listen()` -> `EventBuilder::listenTo()`.

--- Src/Event/EventType.php
+++ Src/Event/EventType.php
@@ -9,8 +9,47 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Lib\Container\Event;
 
+use Closure;
+use LogicException;
+use TheWebSolver\Codegarage\Lib\Container\Container;
+use TheWebSolver\Codegarage\Lib\Container\Pool\Stack;
+
 enum EventType {
 	case BeforeBuild;
 	case Building;
 	case AfterBuild;
+
+	/**
+	 * @param Container $app
+	 * @param string    $entry      Entry can be one of the following based on `EventType`:
+	 * - `EventType::BeforeBuild`
+	 * - `EventType::AfterBuild` -> The `$id` value of `Container::get()` method if no container binding.
+	 *                              Entry/alias of `Container::set()` method if has container binding.
+	 * - `EventType::Building`   -> The parameter type-hint (string, EventBuilder::class, etc).
+	 * @param string    $paramName  The type-hinted parameter name to auto-wire the param value
+	 *                              when `EventType::Building` the given {@param $entry} type.
+	 * @throws LogicException       When param name not passed for `EventType::Building`, or
+	 *                              when concrete is a Closure for `EventType::AfterBuild`.
+	 */
+	public function getKeyFrom( Container $app, string $entry, ?string $paramName ): string {
+		$entry = $app->getEntryFrom( $entry );
+
+		return match ( $this ) {
+			EventType::BeforeBuild => $entry,
+			EventType::Building    => self::assertParamNameProvided( $entry, $paramName ),
+			EventType::AfterBuild  => self::assertConcreteIsString( $app->getConcrete( $entry ), $entry )
+		};
+	}
+
+	private static function assertParamNameProvided( string $entry, ?string $paramName ): string {
+		return ! ! $paramName ? Stack::keyFrom( id: $entry, name: $paramName ) : throw new LogicException(
+			"Parameter name is required when adding event listener during build for entry {$entry}."
+		);
+	}
+
+	private static function assertConcreteIsString( string|Closure $concrete, string $entry ): string {
+		return ! $concrete instanceof Closure ? $concrete : throw new LogicException(
+			"The concrete must be a string when adding event listener during build for entry {$entry}."
+		);
+	}
 }
--- Src/Helper/EventBuilder.php
+++ Src/Helper/EventBuilder.php
@@ -14,7 +14,6 @@ namespace TheWebSolver\Codegarage\Lib\Container\Helper;
 use Closure;
 use LogicException;
 use TheWebSolver\Codegarage\Lib\Container\Container;
-use TheWebSolver\Codegarage\Lib\Container\Pool\Stack;
 use TheWebSolver\Codegarage\Lib\Container\Event\EventType;
 use TheWebSolver\Codegarage\Lib\Container\Interfaces\ListenerRegistry;
 
@@ -28,56 +27,24 @@ final class EventBuilder {
 	) {}
 
 	/**
-	 * @param string $entry     Entry can be one of the following based on `EventType`:
-	 * - `EventType::BeforeBuild`
-	 * - `EventType::AfterBuild` -> The `$id` value of `Container::get()` method if no container binding.
-	 *                              Any value of `Container::set()` method if has container binding.
-	 * - `EventType::Building`   -> The parameter type-hint (string, EventBuilder::class, etc).
-	 * @param string $paramName The dependency parameter name to provide the parameter
-	 *                          value when app is resolving the given {@param $entry}.
-	 * @throws LogicException When param name not passed for `EventType::Building`.
+	 * @throws LogicException When param name not passed for `EventType::Building`, or
+	 *                        when concrete is a Closure for `EventType::AfterBuild`.
 	 */
 	public function for( string $entry, ?string $paramName = null ): self {
-		$this->assertParamNameProvidedWhenResolving( $entry, $paramName );
-
-		$entry = $this->app->getEntryFrom( alias: $entry );
-		$entry = match ( $this->type ) {
-			EventType::BeforeBuild => $entry,
-			EventType::Building    => $this->assertParamNameProvidedWhenResolving( $entry, $paramName ),
-			EventType::AfterBuild  => $this->app->getConcrete( $entry )
-		};
-
-		assert(
-			assertion: ! $entry instanceof Closure,
-			description: 'Concrete is always a string if we reach to After Build event.'
-		);
-
-		$this->key = $paramName ? Stack::keyFrom( id: $entry, name: $paramName ) : $entry;
+		$this->key = $this->type->getKeyFrom( $this->app, $entry, $paramName );
 
 		return $this;
 	}
 
 	/**
-	 * @param Closure(object): void $with
-	 * @throws LogicException When this method is invoked before `EventBuilder::needsListenerFor()` method.
+	 * @param Closure(object): void $listener
+	 * @throws LogicException When this method is invoked before setting entry and/or param name.
 	 */
-	public function listen( Closure $with, int $priority = ListenerRegistry::DEFAULT_PRIORITY ): void {
-		$this->registry->addListener(
-			listener: $with,
-			priority: $priority,
-			forEntry: $this->key ?? throw new LogicException(
-				sprintf( 'Entry not registered. Register using method: %s.', self::class . '::for()' )
-			)
+	public function listenTo( Closure $listener, int $priority = ListenerRegistry::DEFAULT_PRIORITY ): void {
+		$forEntry = $this->key ?? throw new LogicException(
+			sprintf( 'Entry not registered. Register using method: %1$s::%2$s.', self::class, 'for()' )
 		);
-	}
 
-	private function assertParamNameProvidedWhenResolving( string $entry, ?string $paramName ): string {
-		if ( EventType::Building !== $this->type || null !== $paramName ) {
-			return $entry;
-		}
-
-		throw new LogicException(
-			"Parameter name is required when adding event listener during build for entry {$entry}."
-		);
+		$this->registry->addListener( $listener, $forEntry, $priority );
 	}
 }
