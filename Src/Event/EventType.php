4b69cf3aafb4b449713becffd2d2e708be3fedfa
Shesh Ghimire <sheshgh@outlook.com>
2024-11-10 17:12:39 +0545

    Fixed exception msg for after build. Fixed comment.

--- Src/Event/EventType.php
+++ Src/Event/EventType.php
@@ -50,7 +50,7 @@ enum EventType {
 	 * - `EventType::BeforeBuild`
 	 * - `EventType::AfterBuild` -> The `$id` value of `Container::get()` method if no container binding.
 	 *                              Entry/alias of `Container::set()` method if has container binding.
-	 * - `EventType::Building`   -> The parameter type-hint (string, EventBuilder::class, etc).
+	 * - `EventType::Building`   -> The parameter type-hint (string, EventBuilder, etc).
 	 * @param string    $paramName  The type-hinted parameter name to auto-wire the param value
 	 *                              when `EventType::Building` the given {@param $entry} type.
 	 * @throws LogicException       When param name not passed for `EventType::Building`, or
@@ -74,7 +74,7 @@ enum EventType {
 
 	private static function assertConcreteIsString( string|Closure $concrete, string $entry ): string {
 		return ! $concrete instanceof Closure ? $concrete : throw new LogicException(
-			"The concrete must be a string when adding event listener during build for entry {$entry}."
+			"The concrete must be a string when adding event listener after build for entry {$entry}."
 		);
 	}
 }
