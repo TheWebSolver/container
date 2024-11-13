b1b99aa4f5345484f3bbd5949a7cc93d230efe44
Shesh Ghimire <sheshgh@outlook.com>
2024-11-13 11:21:38 +0545

    `Compilable::fromCompiledFile()` @throws tag in doc comment.

--- Src/Interfaces/Compilable.php
+++ Src/Interfaces/Compilable.php
@@ -9,6 +9,8 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Lib\Container\Interfaces;
 
+use RuntimeException;
+
 /**
  * @template TKey
  * @template TValue
@@ -23,6 +25,8 @@ interface Compilable {
 
 	/**
 	 * Gets an instance using a filepath that returns the compiled data.
+	 *
+	 * @throws RuntimeException When given $path is not a valid path.
 	 */
 	public static function fromCompiledFile( string $path ): static;
 }
